f208de66976feb6faf3a0dd9e057f9875b9d0ada
Morshed-17 <morshedmto@gmail.com>
2023-09-14 19:30:52 +0600

    project initialized with tailwind css and google fonts

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Course Registration</h1>
+    </div>
+  )
+}
+
+export default App
